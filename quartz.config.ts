60e18b17db3f679c6a5dceb2744e9fbc6ab0a748
twopieare <skushwaha2706@gmail.com>
2024-05-16 11:41:55 +0530

    Quartz sync: May 16, 2024, 11:41 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "shivammk27.github.io/notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
